ca1fa196312c46c7b4c087f66ea0e545d1ce8e4a
Dima <dmitrijs.visnakovs@inbox.lv>
2024-07-18 07:18:05 +0300

    feat: add styles

--- inctagram/src/main.tsx
+++ inctagram/src/main.tsx
@@ -1,9 +1,13 @@
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
+import "./styles/index.scss";
+
 import { StrictMode } from 'react'
  
 import { createRoot } from 'react-dom/client'
  
 import { App } from './App'
- 
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
---
+++ inctagram/src/stories/preview.ts
@@ -0,0 +1,3 @@
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
+import "../src/styles/index.scss";
